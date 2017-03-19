c9977a9bf01120dfaba2a12be55c0ad8f473b383
zineddine kermouz <zineddine.kermouz@gmail.com>
2017-03-19 20:10:01 +0100

    [Vidly] Initial Commit

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers/Index
+        public ActionResult Index()
+        {
+            List<Customer> customers = new List<Customer>
+            {
+                new Customer { Id=1 , Name = "Zinou" },
+                new Customer {Id = 2, Name = "Mimou" }
+            };
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+
+            return View(customer);
+        }
+
+        public IEnumerable<Customer> GetCustomers()
+        {
+            List<Customer> customers = new List<Customer>
+            {
+                new Customer { Id=1 , Name = "Zinou" },
+                new Customer {Id = 2, Name = "Mimou" }
+            };
+
+            return customers;
+        }
+    }
+}
